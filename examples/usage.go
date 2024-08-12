01d0745f8ab33858a7a1edd194907b31e5885307
Huan Wang <fredwanghuan@gmail.com>
2024-08-11 20:12:35 -0600

    reboot usage

--- examples/usage.go
+++ examples/usage.go
@@ -4,20 +4,49 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/mrmarble/deco"
 )
 
+const ENV_ROUTER_ADDR = "ROUTER_ADDR"
+const ENV_ROUTER_PASSWD = "ROUTER_PASSWD"
+
 func main() {
-	c := deco.New("192.168.1.1")
-	err := c.Authenticate("router_password")
+	var err error
+
+	// sample code to reverse payload from webui
+	// localStorage.getItem('encryptorAES') ==> "k=1723427984598410&i=1723427984598385"
+	// aesKeys := utils.AESKey{}
+	// aesKeys.Key = []byte("1723427984598410")
+	// aesKeys.Iv = []byte("1723427984598385")
+
+	// s, _ := url.QueryUnescape("Q6iL%2FAhNpPCOlppWDiTIbNIIJsDlm0VKuWjF3uvxq8k%3D")
+	// res, err := utils.AES256Decrypt(s, aesKeys)
+	// fmt.Println("res", res)
+	// fmt.Println("err", err)
+	// os.Exit(0)
+
+	c := deco.New(os.Getenv(ENV_ROUTER_ADDR))
+	err = c.Authenticate(os.Getenv(ENV_ROUTER_PASSWD))
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
-	printPerformance(c)
-	printDevices(c)
-	printDecos(c)
+	devices, err := c.DeviceList()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	deviceMacs := []string{}
+	for _, d := range devices.Result.DeviceList {
+		deviceMacs = append(deviceMacs, d.MAC)
+	}
+	fmt.Println(deviceMacs)
+	_, err = c.Reboot(deviceMacs...)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 }
 
 func printPerformance(c *deco.Client) {
@@ -53,4 +82,3 @@ func printDecos(c *deco.Client) {
 		fmt.Printf("%s\tStatus: %s\n", device.DeviceIP, device.InetStatus)
 	}
 }
-
